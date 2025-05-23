76e584587ab3254447e6cee360915f160192f792
Syauqi <sauki084@gmail.com>
2025-05-23 15:28:24 +0700

    Install laravel breeze

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.jsx",
+                // Tambahkan file CSS/JS lain yang kamu gunakan di sini
+            ],
         }),
-        tailwindcss(),
+        react(),
     ],
 });
